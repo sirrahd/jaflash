c61ba62536d1dd093bba1ca26b0c5fea2923d709
Andrew Harris <me@drew-harris.com>
2014-09-13 22:50:41 -0400

    Update with lots of refactoring and starting intermediate book.

--- make.py
+++ make.py
@@ -1,62 +1,40 @@
 import os
+import csv
 
-def kanji(fname, ipath, opath):
-	print("Making kanji " + fname + " in " + opath);
-	r = open(ipath + '/' + fname, encoding='utf-8')
-	w = open(opath + '/' + fname, 'w', encoding='utf-8')
+def formatLine(row, filetype):
+	if filetype == 'kanji':
+		row = [row['kanji'], " ".join([row['kana'], row['definition']])]
+	elif filetype == 'vocab':
+		row = [row['kanji'], row['kana'], row['definition']]
+	return row
 
-	lineCount = 0
-	history = []
-	for line in r:
-		lineCount += 1
-		line = line.strip()
-		if line not in history:
-			history.append(line)
-			line = line.split('\t')
-			try:
-			    history.append(' '.join(line))
-			    w.write(line[1] + '\t' + line[0] + ' ' + line[2] + '\n')
-			except:
-			    print('Error in line ' + str(lineCount) + '.\n')
+def make(outpath, filename, filetype, filedata):
+	with open(os.path.join(outpath, filename), 'w', newline='', encoding='utf-8') as csvfile:
+		writer = csv.writer(csvfile, delimiter='\t')
+		for row in filedata:
+			writer.writerow(formatLine(row, filetype))
+	csvfile.close()
 
-	w.close()
-	r.close()
 
-def vocab(fname, ipath, opath):
-	print("Making vocab " + fname + " in " + opath);
-	r = open(ipath + '/' + fname, encoding='utf-8')
-	w = open(opath + '/' + fname, 'w', encoding='utf-8')
-
-	lineCount = 0
-	for line in r:
-		lineCount += 1
-		output = ['','','']
-		line = line.split('\t')
-	    
-		if line[1] != '':
-			output[0] = line[1]
-			output[1] = line[0]
-		else:
-			output[0] = line[0]
-			output[1] = ''
-
-		try:
-			output[2] = line[2]
-		except:
-			print('Error in line ' + str(lineCount) + '.\n')
-
-		w.write('\t'.join(output))
-
-	w.close()
-	r.close()
+def parse(filepath):
+	data = []
+	with open(filepath, newline='', encoding='utf-8') as csvfile:
+		reader = csv.reader(csvfile, delimiter='\t')
+		for row in reader:
+			dataline = { 'kana': row[0], 'kanji': row[1], 'definition': row[2] }
+			if dataline not in data:
+				data.append(dataline)
+	csvfile.close()
+	return data
+			
 
 for (path, dirs, files) in os.walk('./raw'):
-	fpath = '.' + path[5:]
-	if not os.path.exists(fpath):
-		os.makedirs(fpath)
-	for fname in files:
-		ftype = fname.split('.')[-2]
-		if ftype == 'kanji':			
-			kanji(fname, path, fpath)
-		elif ftype == 'vocab':
-			vocab(fname, path, fpath)
+	outpath = '.' + path.split('./raw')[1]
+	if not os.path.exists(outpath):
+		os.makedirs(outpath)
+	for filename in files:
+		filepath = os.path.join(path, filename)
+		filetype = filename.split('.')[-2]
+		print("Processing " + filepath)
+		filedata = parse(filepath)
+		make(outpath, filename, filetype, filedata)
