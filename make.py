90d4f819da2244350da1145595d6ee61430284b0
Andrew Harris <me@drew-harris.com>
2015-05-28 08:48:32 -0400

    New loading methods and relationship manager

--- make.py
+++ make.py
@@ -5,96 +5,87 @@ import os
 import sys
 from models import Word
 
-recordsCount = len(Word.getAll())
-existsRecords = 0
-newRecords = 0
-
 def makeComboItem(word1, word2):
-	global newRecords
-	
-	notDone = True
-	while(notDone):
-		print("Make an entry to combine the following words:")
-		print(word1)
-		print(word2)
-		kana = input('kana>')
-		kanji = input('kanji>')
-		definition = input('definition>')
-		
-		word = Word(kana = kana, kanji = kanji, definition = definition)
-		print(word)
-		if 'y' == input("Is this correct? (y/n) "):
-			notDone = False
-			word.save()
-			newRecords += 1
-			word1.owner = word
-			word1.save()
-			word2.owner = word
-			word2.save()
+    word = Word()
+    while(word.id == None):
+        print("Make an entry to combine the following words:")
+        print(word1)
+        print(word2)
+        kana = input('kana>')
+        kanji = input('kanji>')
+        definition = input('definition>')
+        
+        word = Word(kana = kana, kanji = kanji, definition = definition)
+        print(word)
+        if 'y' == input("Is this correct? (y/n) "):
+            word.save()
+            word1.dupeOf(word)
+            word2.dupeOf(word)
+            
+    return word
 
-def resolveDuplicates(word):
-	global recordsCount
-	global existsRecords
-	global newRecords
-	
-	print(str(recordsCount) + " starting records")
-	print(str(existsRecords) + " already exist, " + str(newRecords) + " new records")
-	
-	dupeList = []
-	i = 0
-	for item in word.getPotentialDuplicates():
-		print(str(i + 1) + "\t" + str(item))
-		i += 1
-		dupeList.append(item)
-	print("Item:\t" + str(word))
-	print('Select item this is (d)uplicate or (o)wner of, (m)ake a new item combining two, (c)ontinue, or (q)uit')
-	while(True):
-		command = input('>')
-		command = command.strip()
-		if command[0] == 'd':
-			word.owner = dupeList[int(command[1:]) - 1]
-			break
-		elif command[0] == 'o':
-			dupeList[int(command[1:]) - 1].owner = word
-			dupeList[int(command[1:]) - 1].save()
-			break
-		elif command[0] == 'm':
-			makeComboItem(word, dupeList[int(command[1:]) - 1])
-			break
-		elif command[0] == 'c':
-			break
-		elif command[0] == 'q':
-			sys.exit()
-	word.save()
+def resolveDuplicates(word, progressCount):
+    global originalTotalCount
+    global originalDupeCount
+    global originalRelationshipCount
+    
+    if word.owner_id != None:
+        return;
+    
+    dupes = word.getPotentialDuplicates()    
+    while(len(dupes)):
+        print(str(progressCount) + " of " + str(Word.totalCount()))
+        print(str(Word.totalCount() - originalTotalCount - Word.dupeCount() + originalDupeCount) + " new originals, " + str(Word.dupeCount() - originalDupeCount) + " new dupes, " + str(Word.relationshipCount() - originalRelationshipCount) + " new relationships")
+        i = 1
+        for item in dupes:
+            print(str(i) + "\t" + str(item))
+            i += 1
+        print("Item:\t" + str(word))
+        print('Select item this is (d)uplicate or (o)wner of, (m)ake a new item combining two, (c)ontinue, or (q)uit')
+        command = input('>').strip()
+        
+        if command[0] == 'd':
+            word.dupeOf(dupes[int(command[1:]) - 1])
+            word = dupes[int(command[1:]) - 1]
+        elif command[0] == 'o':
+            word.ownerOf(dupes[int(command[1:]) - 1])
+        elif command[0] == 'm':
+            word = makeComboItem(word, dupes[int(command[1:]) - 1])
+        elif command[0] == 'c':
+            for dupe in dupes:
+                word.resolve(dupe)
+        elif command[0] == 'q':
+            sys.exit()
+        dupes = word.getPotentialDuplicates()
 
-def parse(filepath, folder, chapter, section):
-	global recordsCount
-	global existsRecords
-	global newRecords
-	
-	data = []
-	with open(filepath, newline='', encoding='utf-8') as csvfile:
-		reader = csv.reader(csvfile, delimiter='\t')
-		for row in reader:
-			w = Word(kana = row[0], kanji = row[1], definition = row[2], book = folder, chapter = chapter, section = section)
-			if w.alreadyExists():
-				existsRecords += 1
-				continue
-			elif w.isDupe():
-				resolveDuplicates(w)
-				newRecords += 1
-			else:
-				newRecords += 1
-				w.save()
+def populate(filepath, folder, chapter, section):
+    data = []
+    with open(filepath, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            Word(kana = row[0], kanji = row[1], definition = row[2], book = folder, chapter = chapter, section = section).save()
+
+originalTotalCount = Word.totalCount()
+originalDupeCount = Word.dupeCount()
+originalRelationshipCount = Word.relationshipCount()
 
 for (path, dirs, files) in os.walk('./vocab'):
-	book = path.split('./vocab')[1][1:]
-	for filename in files:
-		filepath = os.path.join(path, filename)
-		chapter = str(int(filename.split('.')[0]))
-		section = filename.split('.')[1]
-		filedata = parse(filepath, book, chapter, section)
-	
-print("All done building database. Results:")
-print(str(recordsCount) + " starting records")
-print(str(existsRecords) + " already exist, " + str(newRecords) + " new records")
+    book = path.split('./vocab')[1][1:]
+    for filename in files:
+        filepath = os.path.join(path, filename)
+        chapter = str(int(filename.split('.')[0]))
+        section = filename.split('.')[1]
+        populate(filepath, book, chapter, section)
+    
+print("Database synced")
+print(str(Word.totalCount() - originalTotalCount) + " new records, " + str(Word.totalCount()) + " total records")
+
+i=1
+for word in Word.getAll():
+    resolveDuplicates(word, i)
+    i += 1
+    
+print("Duplicates resolved")
+print(str(Word.totalCount() - originalTotalCount - Word.dupeCount() + originalDupeCount) + " new originals, " + str(Word.dupeCount() - originalDupeCount) + " new dupes, " + str(Word.relationshipCount() - originalRelationshipCount) + " new relationships")
+print(str(Word.totalCount()) + " total words, " + str(Word.totalCount() - Word.dupeCount()) + " total originals, " + str(Word.dupeCount()) + " total dupes, " + str(Word.relationshipCount()) + " total relationships")
+
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import create_engine, ForeignKey, UniqueConstraint
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import path, getcwd
@@ -8,68 +7,115 @@ from os import path, getcwd
 engine = create_engine('sqlite:///' + path.join(getcwd(), 'data.db'))
 Base = declarative_base()
 
-class Word(Base):
-	__tablename__ = 'words'
-	
-	id = Column(Integer, primary_key=True)
-	kana = Column(String)
-	kanji = Column(String)
-	definition = Column(String)
-	book = Column(String)
-	chapter = Column(String)
-	section = Column(String)
-	owner_id = Column(Integer, ForeignKey('words.id'))
-	owner = relationship('Word', backref='parent', remote_side='Word.id')
-	
-	def __init__(self, kana=None, kanji=None, definition=None, book=None, chapter=None, section=None):
-		self.kana = kana
-		self.kanji = kanji
-		self.definition = definition
-		self.book = book
-		self.chapter = chapter
-		self.section = section
-	
-	def __repr__(self):
-		return "<Word(kana='%s', kanji='%s', definition='%s', book='%s', chapter='%s', section='%s')>" % (self.kana, self.kanji, self.definition, self.book, self.chapter, self.section)
-	
-	def save(self):
-		session.add(self)
-		session.commit()
+class Resolution(Base):
+    __tablename__ = 'resolutions'
+    
+    word1_id = Column(Integer, ForeignKey('words.id'), primary_key=True)
+    word2_id = Column(Integer, ForeignKey('words.id'), primary_key=True)
+    UniqueConstraint('word1_id', 'word2_id')
+    
+    def __init__(self, word1, word2):
+        self.word1_id = word1.id
+        self.word2_id = word2.id
+        
+    def save(self):
+        session.add(self)
+        session.commit()
 
-	def isDupe(self):
-		if self.owner:
-			return True
-		elif len(self.getPotentialDuplicates()):
-			return True
-		else:
-			return False
-			
-	def getDupe(self):
-		if self.owner_id:
-			return getById(self.duplicate_id)
-		else:
-			return None
-	
-	def alreadyExists(self):
-		if session.query(Word).filter(Word.kana == self.kana, Word.kanji == self.kanji, Word.definition == self.definition).count() > 0:
-			return True
-		else:
-			return False
-		
-	def getPotentialDuplicates(self):
-		dupes = session.query(Word).filter(Word.kana != None, Word.kana.contains(self.kana), Word.owner_id == None)
-		if self.kanji != '':
-			dupes.union(session.query(Word).filter(Word.kanji != None, Word.kanji.contains(self.kanji), Word.owner_id == None))
-		if self.definition != '':
-			dupes.union(session.query(Word).filter(Word.definition != None, Word.definition.contains(self.definition), Word.owner_id == None))
-		
-		return dupes.all()
-		
-	def getAll():
-		return session.query(Word).all()
-		
-	def getById(wordId):
-		return session.query(Word).filter(Word.id == wordId).one()
+    def alreadyExists(self):
+        if self.id == None and session.query(Resolution).filter(Resolution.word1_id == self.word1.id, Resolution.word2_id == self.word2.id).union(
+                               session.query(Resolution).filter(Resolution.word1_id == self.word2.id, Resolution.word2_id == self.word1.id)).count() > 0:
+            return True
+        else:
+            return False
+    
+class Word(Base):
+    __tablename__ = 'words'
+    
+    id = Column(Integer, primary_key=True)
+    kana = Column(String)
+    kanji = Column(String)
+    definition = Column(String)
+    book = Column(String)
+    chapter = Column(String)
+    section = Column(String)
+    owner_id = Column(Integer, ForeignKey('words.id'))
+
+    def __init__(self, kana=None, definition=None, kanji=None, book=None, chapter=None, section=None):
+        self.kana = kana
+        self.kanji = kanji
+        self.definition = definition
+        self.book = book
+        self.chapter = chapter
+        self.section = section
+    
+    def __repr__(self):
+        return "<Word(kana='%s', kanji='%s', definition='%s', book='%s', chapter='%s', section='%s')>" % (self.kana, self.kanji, self.definition, self.book, self.chapter, self.section)
+    
+    def save(self):
+        if self.alreadyExists():
+            return
+        session.add(self)
+        session.commit()
+        
+    def ownerOf(self, other):
+        other.owner_id = self.id
+        other.save()
+    
+    def dupeOf(self, other):
+        self.owner_id = other.id
+        self.save()
+        
+    def resolve(self, other):
+        r = Resolution(self, other)
+        r.save()
+
+    def isResolved(self, otherId):
+        if self.id == otherId:
+            return True
+            
+        q = session.query(Resolution).filter(Resolution.word1_id == self.id, Resolution.word2_id == otherId).union(
+            session.query(Resolution).filter(Resolution.word1_id == otherId, Resolution.word2_id == self.id))
+        
+        if q.count() > 0:
+            return True
+        else:
+            return False
+    
+    def alreadyExists(self):
+        if self.id == None and session.query(Word).filter(Word.kana == self.kana, Word.kanji == self.kanji, Word.definition == self.definition).count() > 0:
+            return True
+        else:
+            return False
+        
+    def getPotentialDuplicates(self):
+        dupes = session.query(Word).filter(Word.kana != None, Word.kana.contains(self.kana), Word.owner_id == None).union(
+                session.query(Word).filter(Word.kanji != None, Word.kanji.contains(self.kanji), Word.owner_id == None),
+                session.query(Word).filter(Word.definition != None, Word.definition.contains(self.definition), Word.owner_id == None)).all()
+        
+        trueDupes = []
+        for dupe in dupes:
+            if self.isResolved(dupe.id):
+                continue
+            else:
+                trueDupes.append(dupe)
+        
+        return trueDupes
+        
+    def getAll():
+        return session.query(Word).all()
+        
+    def totalCount():
+        return session.query(Word).count()
+        
+    def dupeCount():
+        return session.query(Word).filter(Word.owner_id != None).count()
+        
+    def relationshipCount():
+        return session.query(Resolution).count()
+        
+    def getById(wordId):
+        return session.query(Word).filter(Word.id == wordId).one()
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
